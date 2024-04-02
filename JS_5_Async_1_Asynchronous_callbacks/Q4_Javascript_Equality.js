02e44b8403248eb89dc953e9a7398b9f42b1db75
Gokulabala <gokul.ncrc@gmail.com>
2024-04-02 13:06:36 +0530

    Async Additional  Problems

---
+++ JS_5_Async_1_Asynchronous_callbacks/Q4_Javascript_Equality.js
@@ -0,0 +1,5 @@
+// Q2. Javascript equality
+// What is the difference between '==' and '===' operator in Javascript.
+
+// //solution
+// '==' only checks for the value and coerce type while '===' checks for both type and value.
